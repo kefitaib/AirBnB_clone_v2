58d59a55f104cb73a72b985aaa9cb5cb05b50743
kefitaib <taibkefi@yahoo.fr>
2020-08-13 06:41:23 -0700

    test console

--- tests/test_console.py
+++ tests/test_console.py
@@ -15,16 +15,30 @@ class TestConsole(unittest.TestCase):
         with patch('sys.stdout', new=StringIO()) as f:
             HBNBCommand().onecmd("create")
         s = f.getvalue()[:-1]
-        self.assertEqual(s, "* class name missing *")
+        self.assertEqual(s, "** class name missing **")
 
     def test_false_class(self):
         with patch('sys.stdout', new=StringIO()) as f:
             HBNBCommand().onecmd("create nob")
         s = f.getvalue()[:-1]
-        self.assertEqual(s, "* class doesn't exist *")
+        self.assertEqual(s, "** class doesn't exist **")
 
     def test_exist_class(self):
         with patch('sys.stdout', new=StringIO()) as f:
             HBNBCommand().onecmd("create State")
         s = f.getvalue()[:-1]
         self.assertEqual(len(s), 36)
+
+    def test_room_nbr(self):
+        with patch('sys.stdout', new=StringIO()) as f:
+            HBNBCommand().onecmd('create State number_rooms=2')
+            HBNBCommand().onecmd('all State')
+        s = f.getvalue()
+        self.assertTrue("'number_rooms': 2" in s)
+
+    def test_longitude(self):
+        with patch('sys.stdout', new=StringIO()) as f:
+            HBNBCommand().onecmd('create State longitude=-12.43789')
+            HBNBCommand().onecmd('all State')
+        s = f.getvalue()
+        self.assertTrue("longitude': -12.43789" in s)
