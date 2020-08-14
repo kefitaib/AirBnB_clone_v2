074dc3636a39e1b3f865d33923973c2cb1c627d2
hunterxx0 <azouzimhamed@gmail.com>
2020-08-14 11:49:08 +0000

    first

--- models/state.py
+++ models/state.py
@@ -1,26 +1,23 @@
 #!/usr/bin/python3
 """ State Module for HBNB project """
 from models.base_model import BaseModel, Base
-from sqlalchemy import (create_engine)
-from sqlalchemy.orm import sessionmaker
-from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy import Column, Integer, String, DateTime, ForeignKey
 from sqlalchemy.orm import relationship
+from os import getenv
 import models
+
 
 class State(BaseModel, Base):
     """ State class """
 
     __tablename__ = 'states'
-    super.__init__
     name = Column(String(128), nullable=False)
     cities = relationship("City", backref="state", cascade="all, delete")
 
-    @property
-    def cities(self):
-        """ return city """
-
-        from models import storage
-
-        l = storage.all('City')
-        return [v for k, v in l.items() if v.state_id == self.id]
+    if getenv('HBNB_TYPE_STORAGE') != 'db':
+        @property
+        def cities(self):
+            """ return city """
+            from models import storage
+            l = storage.all('City')
+            return [v for k, v in l.items() if v.state_id == self.id]
