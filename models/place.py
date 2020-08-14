0b9a984ddc7ae6571117d68eeaf473abc6912bdd
kefitaib <taibkefi@yahoo.fr>
2020-08-14 15:29:04 -0700

    fix

--- models/place.py
+++ models/place.py
@@ -3,6 +3,7 @@
 from models.base_model import BaseModel, Base
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy import Column, Integer, String, Table, ForeignKey, Float
+from sqlalchemy import *
 from os import getenv
 from models.review import Review
 from models.amenity import Amenity
