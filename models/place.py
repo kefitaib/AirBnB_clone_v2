4c69e79dea814fbda6071cb4c7ea0f131bda76de
kefitaib <taibkefi@yahoo.fr>
2020-08-14 15:24:31 -0700

    fix

--- models/place.py
+++ models/place.py
@@ -8,8 +8,8 @@ from os import getenv
 from models.review import Review
 from models.amenity import Amenity
 from sqlalchemy.orm import relationship
-metadata = Base.metadata
-place_amenity = Table('place_amenity', metadata,
+
+place_amenity = Table('place_amenity', Base.metadata,
                       Column('place_id', String(60), ForeignKey('places.id'),
                              primary_key=True, nullable=False),
                       Column('amenity_id', String(60),
