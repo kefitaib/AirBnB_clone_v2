b85dc2b0169f83c75ba2f150f5493d9e35d88467
kefitaib <taibkefi@yahoo.fr>
2020-09-02 05:51:43 -0700

    starts a Flask web application with varible test

--- web_flask/4-number_route.py
+++ web_flask/4-number_route.py
@@ -37,12 +37,11 @@ def display_Python(text='is cool'):
     return 'Python ' + text
 
 
-@app.route('/number/<n>', strict_slashes=False)
+@app.route('/number/<int:n>', strict_slashes=False)
 def is_number(n):
     """ function """
 
-    if type(n) is int:
-        return '{} is a number'.format(n)
+    return '{} is a number'.format(n)
 
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port=5000)
