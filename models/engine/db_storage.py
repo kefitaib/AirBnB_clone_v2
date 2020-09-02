0e38f80150f4757b655767004422e6572f99313c
kefitaib <taibkefi@yahoo.fr>
2020-09-02 06:45:35 -0700

    update some part to be able to display our HBNB data using Flask

--- models/engine/db_storage.py
+++ models/engine/db_storage.py
@@ -82,4 +82,4 @@ class DBStorage:
     def close(self):
         """ call remove() method on the private session attribute session """
 
-        self.__session.close
+        self.__session.close()
