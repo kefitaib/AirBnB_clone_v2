a0ef5b0b49e6f2a11df7ba019f2147cc2d08fd11
kefitaib <taibkefi@yahoo.fr>
2020-09-02 06:23:41 -0700

    starts a Flask web application with rendering templates

--- web_flask/6-number_odd_or_even.py
+++ web_flask/6-number_odd_or_even.py
@@ -51,7 +51,7 @@ def template(n):
     return render_template('5-number.html', n=n)
 
 
-@app.route('/number_template/<int:n>', strict_slashes=False)
+@app.route('/number_odd_or_even/<int:n>', strict_slashes=False)
 def template_6(n):
     """ function """
 
